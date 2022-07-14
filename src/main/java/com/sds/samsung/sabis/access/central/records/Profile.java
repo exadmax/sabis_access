aa1329d9da8025a071ba0e0132440b78e12ed076
Adalberto <102322394+amendesc@users.noreply.github.com>
2022-07-14 16:31:14 -0400

    Added entities
    Added entities:
    - Profile;
    - Rule;
    - User;
    - WeekdayRule.

--- src/main/java/com/sds/samsung/sabis/access/central/records/Profile.java
+++ src/main/java/com/sds/samsung/sabis/access/central/records/Profile.java
@@ -2,13 +2,90 @@ package com.sds.samsung.sabis.access.central.records;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity 
+@Table(name = "PROFILE") 
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Profile {
-	private Long id;
-	private String name;
-	private List<Rule> rules;
-	private Boolean approver;
-	private Boolean gpsRestricted;
-	private Boolean needCheckin;
 	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@Column(name = "PRF_ID", updatable = false, nullable = false)
+	private Long id;
+	
+	@Column(name = "PRF_NAME", nullable = false)
+	private String name;
+	
+	@OneToMany(mappedBy = "Rule", cascade = CascadeType.ALL, orphanRemoval = true) 
+	@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+	private List<Rule> rules;
+	
+	@Column(name = "PRF_APPROVER")
+	private Boolean approver;
+	
+	@Column(name = "PRF_GPSRESTRICTED")
+	private Boolean gpsRestricted;
+	
+	@Column(name = "PRF_NEEDCHECKIN")
+	private Boolean needCheckin;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Rule> getRules() {
+		return rules;
+	}
+
+	public void setRules(List<Rule> rules) {
+		this.rules = rules;
+	}
+
+	public Boolean getApprover() {
+		return approver;
+	}
+
+	public void setApprover(Boolean approver) {
+		this.approver = approver;
+	}
+
+	public Boolean getGpsRestricted() {
+		return gpsRestricted;
+	}
+
+	public void setGpsRestricted(Boolean gpsRestricted) {
+		this.gpsRestricted = gpsRestricted;
+	}
+
+	public Boolean getNeedCheckin() {
+		return needCheckin;
+	}
+
+	public void setNeedCheckin(Boolean needCheckin) {
+		this.needCheckin = needCheckin;
+	}
 	
 }
--- src/main/java/com/sds/samsung/sabis/access/central/records/Rule.java
+++ src/main/java/com/sds/samsung/sabis/access/central/records/Rule.java
@@ -2,12 +2,79 @@ package com.sds.samsung.sabis.access.central.records;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity 
+@Table(name = "RULE") 
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Rule {
-	private Long id;
-	private String name;
-	private List<WeekdayRule> weekDays;
-	private Boolean approver;
-	private Boolean gpsRestricted;
 	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@Column(name = "RUL_ID", updatable = false, nullable = false)
+	private Long id;
+	
+	@Column(name = "RUL_NAME")
+	private String name;
+	
+	@OneToMany(mappedBy = "WeekdayRule", cascade = CascadeType.ALL, orphanRemoval = true) 
+	@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+	private List<WeekdayRule> weekDays;
+	
+	@Column(name = "RUL_APPROVER")
+	private Boolean approver;
+	
+	@Column(name = "RUL_RESTRICTED")
+	private Boolean gpsRestricted;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<WeekdayRule> getWeekDays() {
+		return weekDays;
+	}
+
+	public void setWeekDays(List<WeekdayRule> weekDays) {
+		this.weekDays = weekDays;
+	}
+
+	public Boolean getApprover() {
+		return approver;
+	}
+
+	public void setApprover(Boolean approver) {
+		this.approver = approver;
+	}
+
+	public Boolean getGpsRestricted() {
+		return gpsRestricted;
+	}
+
+	public void setGpsRestricted(Boolean gpsRestricted) {
+		this.gpsRestricted = gpsRestricted;
+	}
 	
 }
