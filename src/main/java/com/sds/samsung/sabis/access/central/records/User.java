160946ff94f4d0164037896ab6a588b901b0277a
Diogo Góes Zanetti <exadmax@gmail.com>
2022-07-14 15:35:07 -0300

    novos parametros user

--- src/main/java/com/sds/samsung/sabis/access/central/records/User.java
+++ src/main/java/com/sds/samsung/sabis/access/central/records/User.java
@@ -5,7 +5,9 @@ public class User {
 	private Long id;
 	private String firstName;
 	private String lastName;
+	private String matricula;
+	private String email;
+	private Boolean approver;
 	private Profile profile;
-	
 	
 }
